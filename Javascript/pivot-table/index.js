f648d09bcdc1fc62e9450c4028f28c4297040b31
yashvanth <yashvanth.ganesan@syncfusion.com>
2023-01-31 13:45:17 +0530

    documentation(EJ2-68291): To include UG for MySql data binding for EJ2 Pivot Table

--- Javascript/pivot-table/index.js
+++ Javascript/pivot-table/index.js
@@ -1,12 +1,12 @@
 var pivotObj = new ej.pivotview.PivotView({
-	dataSourceSettings: {
+  dataSourceSettings: {
     url: 'https://localhost:7146/Pivot',
     columns: [{ name: 'ShipName' }],
     values: [{ name: 'Freight', caption: 'Sum of Freight' },],
     rows: [{ name: 'ShipCity' }],
-	},
-	showFieldList: true,
-	width: '100%',
+  },
+  showFieldList: true,
+  width: '100%',
   height: 350,
 });
 pivotObj.appendTo('#PivotView');
