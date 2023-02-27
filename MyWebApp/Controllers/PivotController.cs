ff6d08b36b4fd148c10c54d276f46c8d1b11d88e
SasthaPrathap <sasthaprathap.selvamoorthy@syncfusion.com>
2023-02-27 19:52:04 +0530

    documentation(EJ2-68530): Updating UG samples.

---
+++ MyWebApp/Controllers/PivotController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
+using System.Data;
+
+namespace MyWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PivotController : ControllerBase
+    {
+        [HttpGet(Name = "GetMySQLResult")]
+        public object Get()
+        {
+            return JsonConvert.SerializeObject(GetMySQLResult());
+        }
+
+        public dynamic GetMySQLResult()
+        {
+            MySqlConnection connection = new MySqlConnection("<Enter your valid connection string here>");
+            connection.Open();
+            MySqlCommand command = new MySqlCommand("SELECT * FROM orders", connection);
+            MySqlDataAdapter dataAdapter = new MySqlDataAdapter(command);
+            DataTable dataTable = new DataTable();
+            dataAdapter.Fill(dataTable);
+            connection.Close();
+            return dataTable;
+        }
+    }
+}
